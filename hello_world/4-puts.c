eb1d82ecf9496e4b6aad323965033a10008d23ff
Ho2bes <8696@holbertonstudents.com>
2024-02-19 10:58:06 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 int main ()
 {
-  puts ("Programming is like building a multilingual puzzle");
+  /* puts command */
+  
+  puts ("Programming is like building a multilingual puzzle);
   
   return 0;
     }
