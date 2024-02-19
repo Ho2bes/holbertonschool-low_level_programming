5b85186d414390ff40ef0da230fd9e3f443fb3c3
Ho2bes <8696@holbertonstudents.com>
2024-02-19 14:17:24 +0000

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,7 +11,7 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if(n > 0)
 { printf("%d, is positive\n", n) };
-if else(n == 0)
+else if(n == 0)
 { printf("%d, is zero\n", n) };
 else
   { ("%d, is negative\n", n) };
