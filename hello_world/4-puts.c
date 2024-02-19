cd34f9aac9237b3989ee8b5f047221fe1bb1f398
Ho2bes <8696@holbertonstudents.com>
2024-02-19 11:22:40 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
-/* puts command */
-int main(void) /* main of puts command */
+/* 
+*main - puts command
+*return 
+*/
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
