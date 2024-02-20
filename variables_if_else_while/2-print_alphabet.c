4db3c334282aeeea5492bede4128e66f136e0505
Ho2bes <8696@holbertonstudents.com>
2024-02-20 09:40:57 +0000

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+for(ch = 'a' ; ch <= 'z' ; ch++)
+{ putchar(ch);
+}
+return (0);
+}
