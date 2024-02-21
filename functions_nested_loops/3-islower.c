f1cf307c53a081bf5ee73cc9af2f129c3e53d1ca
Ho2bes <8696@holbertonstudents.com>
2024-02-21 15:17:32 +0000

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_islower - Entry point
+*
+*Return: always 0 (Success)
+*/
+
+int _islower(int c)
+{
+
+int c;
+if(c => 61)
+_putchar('1');
+Else
+_putchar('0');
+return (0);
+}
