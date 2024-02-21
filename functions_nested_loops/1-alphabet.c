e1b55ad923b609ad92e5d91fe1bd7fd4cb317f65
Ho2bes <8696@holbertonstudents.com>
2024-02-21 11:38:15 +0000

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: always 0 (Success)
+*/
+
+int main(void)
+{
+char ch
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+return (0);
+}
