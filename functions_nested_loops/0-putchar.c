6bca801dcee9e29e2710889905d0f2f575a02bd1
Ho2bes <8696@holbertonstudents.com>
2024-02-21 09:31:34 +0000

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - Entry point
+*
+*Return: always 0 (Success)
+*/
+
+int main(void)
+{
+char _putchar;
+printf("_printchar\n");
+return (0);
+}
