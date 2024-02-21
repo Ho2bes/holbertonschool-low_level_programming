f5a416226a639e6ea153aff54447f5f22c3231ff
Ho2bes <8696@holbertonstudents.com>
2024-02-21 08:25:38 +0000

    8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int main(void)
 {
 int numero;
 char ch;
-for (numero = '1'; numero <= '9'; numero++)
+for (numero = '0'; numero <= '9'; numero++)
 putchar(numero);
 for (ch = 'a'; ch <= 'f'; ch++)
 putchar(ch);
