7d04e16d2676b44b10af388f12ece6297e43b1b6
Ho2bes <8696@holbertonstudents.com>
2024-02-21 08:21:35 +0000

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: always 0 (Success)
+*/
+
+int main(void)
+{
+int numero; char ch;
+for (numero = '1'; numero <= '9'; numero++)
+putchar(numero);
+for (ch = 'a'; ch <= 'f'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
+    
