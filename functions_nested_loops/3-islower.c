c785f07228152592569f49a514ca6ee8654459dc
Ho2bes <8696@holbertonstudents.com>
2024-02-22 08:33:17 +0000

    3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,16 +2,14 @@
 
 /**
 *_islower - Entry point
-*@c: whaaaaaat
+*@c: -  whaaaaaat
 *Return: always 0 (Success)
 */
 
 int _islower(int c)
 {
-c = 0;
 if (c >= 97 && c <= 122)
-{
 return (1);
-}
+else
 return (0);
 }
