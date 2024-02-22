2cd7f43ad12d512dcd67863e1004aec77dde7132
Ho2bes <8696@holbertonstudents.com>
2024-02-22 12:10:14 +0000

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+    /**
+    *_abs - Entry point
+    *
+    *Return: always 0 (Success)
+    */
+
+int _abs(int);
+
+{
+	int value;
+int absoluteValue;
+
+absoluteValue = abs( Value );
+
+retun (0);
+}
