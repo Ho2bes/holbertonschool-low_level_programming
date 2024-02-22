c297b0fcfe477738a6677969e40471a5519c0af1
Ho2bes <8696@holbertonstudents.com>
2024-02-22 12:54:21 +0000

    Corrected function in main.h

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,18 +1,14 @@
 #include "main.h"
 
  /**
- *_abs - Compute the absolute value of an integer
- *@int: integer to be checked
- *Return: The absolute value
+ * _abs - Compute the absolute value of an integer
+ * @int: integer to be checked
+ * Return: The absolute value
  */
 int _abs(int n)
 {
-
-	if (n >= 0)
-		return (n);
+	if (n < 0)
+		return (-n);
 	else
-	{
-		n = (n * -1);
 		return (n);
-	}
 }
