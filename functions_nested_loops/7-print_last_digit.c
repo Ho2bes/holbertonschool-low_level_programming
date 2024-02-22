4e658ba87e901a6d3befc90a55ec08784e017895
Ho2bes <8696@holbertonstudents.com>
2024-02-22 15:17:50 +0000

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*print_last_digit - Entry point
+*
+*Return: always 0 (Success)
+*/
+
+int print_last_digit(int num)
+{
+	int num;
+	digit = num % 10;
+	return (digit);
+}
