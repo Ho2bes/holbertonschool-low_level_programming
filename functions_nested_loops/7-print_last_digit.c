7625ad09e9cff5725e98dfe46c67e4c7d5c66328
Ho2bes <8696@holbertonstudents.com>
2024-02-23 08:34:41 +0000

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -12,7 +12,8 @@ int print_last_digit(int n)
 int last_digit = n % 10;
 if (last_digit < 0)
 {
-_putchar(last_digit * -1);
+	last_digit = last_digit * -1;
 }
+_putchar('0' + last_digit);
 return (last_digit);
 }
