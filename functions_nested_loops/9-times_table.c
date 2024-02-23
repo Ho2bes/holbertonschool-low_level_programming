5176c8f85d46be942f44eec910f1ab47681a9e50
Ho2bes <8696@holbertonstudents.com>
2024-02-23 14:08:21 +0000

    9-times_table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,31 +7,30 @@
  */
 void times_table(void)
 {
-    int x, y, z;
+int x, y, z;
+for (x = 0; x < 10; x++)
+{
+for (y = 0; y < 10; y++)
+{
+z = y * x;
 
-    for (x = 0; x < 10; x++)
-    {
-        for (y = 0; y < 10; y++)
-        {
-            z = y * x;
+if (y != 0)
+{
+_putchar(',');
+_putchar(' ');
 
-            if (y != 0)
-            {
-                _putchar(',');
-                _putchar(' ');
+if (z < 10)
+_putchar(' ');
+}
 
-                if (z < 10)
-                    _putchar(' ');
-            }
-
-            if (z < 10)
-                _putchar(z + '0');
-            else
-            {
-                _putchar((z / 10) + '0');
-                _putchar((z % 10) + '0');
-            }
-        }
-        _putchar('\n');
-    }
+if (z < 1)
+_putchar(z + '0');
+else
+{
+_putchar((z / 10) + '0');
+_putchar((z % 10) + '0');
+}
+}
+_putchar('\n');
+}
 }
