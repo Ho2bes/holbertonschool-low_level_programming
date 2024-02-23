fe2a9eab2d75350e321fd0199ab8ce73ec66d165
Ho2bes <8696@holbertonstudents.com>
2024-02-23 10:22:06 +0000

    9-times_table.c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+    /**
+    *print_alphabet - Entry point
+    *
+    *Return: always 0 (Success)
+    */
+
+void times_table(void)
+
+{
+	{
+	int x;
+	int y;
+	
+	for (x = 0; x <=9; x++)
+		
+	
+		for (y = 0; y <=9; y++)
+			_putchar ((x * y) + '0');
+			_putchar (',');
+			_putchar (' ');
+			_putchar (' ');
+	}
+			if ((x * 9) && (y * 9))
+			{
+				_putchar ('\n');
+				else
+					return(void);
+	}
+}
