2ae42f97e3fac92c0744de222ddb8984505265ae
Ho2bes <8696@holbertonstudents.com>
2024-02-23 12:30:12 +0000

    9-times_table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,43 +1,44 @@
 #include "main.h"
 
-    /**
-    *print_alphabet - Entry point
-    *
-    *Return: always 0 (Success)
-    */
+/**
+*times_table - Entry point
+*@x : variable 1
+*@y : variable 2
+*@z : variable 3
+*Return: always 0 (Success)
+*/
 
 void times_table(void)
 
 {
-	
-		int x;
-		int y;
-		int z;
+int x;
+int y;
+int z;
 		{
-	for (x = 0; x < 10; x++)
-		for (y = 0; y < 10; y++)
-		       
-		z = x * y;
-			if (x == 0)
-			{
-				_putchar( z + '0');
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
-	if (z < 9 && x != 0 )
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-	}
+for (x = 0; x < 10; x++)
+for (y = 0; y < 10; y++)
 
-	else if (z >= 9)
-	{
-		_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-	}
-	}
-	_putchar('\n');
+z = x * y;
+if (x == 0)
+{
+_putchar( z + '0');
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+			}
+if (z < 9 && x != 0 )
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+
+else if (z >= 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+
+}
+_putchar('\n');
 }
