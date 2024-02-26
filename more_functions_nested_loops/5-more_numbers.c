0c6edf40d79f6b3e158bdc8b3c621619fbaf956c
Ho2bes <8696@holbertonstudents.com>
2024-02-26 11:46:45 +0000

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*more_numbers - Compute the absolute value of an integer
+*
+* Return: The absolute value
+*/
+
+void more_numbers(void)
+{
+	int x;
+	int compteur = 0;
+
+	while (compteur < 10)
+	{
+	for (x = 0; x <= 14; x++)
+	{
+		_putchar(x + '0');
+	}
+	compteur ++;
+	}
+	_putchar('\n');
+		}
