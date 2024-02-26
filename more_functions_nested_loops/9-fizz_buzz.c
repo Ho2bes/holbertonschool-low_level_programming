b1822ee6307905576e7ef0d589d2700129b9abb3
Ho2bes <8696@holbertonstudents.com>
2024-02-26 15:26:35 +0000

    9-fizz_buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
-*print_diagonal - Compute the absolute value of an integer
+*main fizzbuzz - Compute the absolute value of an integer
 *@n : - variable
 * Return: The absolute value
 */
@@ -10,22 +10,21 @@
 
 int main (void)
 {
-    int n;
-    
-    for (n = 0; n <= 100; n++)
-    {
-        if (n % 3 == 0 && n % 5 == 0)
-            printf("FizzBuzz");
-        else if (n % 3 == 0)
-            printf("Fizz");
-        else if (n % 5 == 0)
-            printf("Buzz");
-        else
-            printf("%d", n);
-	if (n != 100)
-		printf(" ");
-    }
-    printf("\n");
+int n;
 
-    return 0;
+for (n = 1; n <= 100; n++)
+{
+if (n % 3 == 0 && n % 5 == 0)
+printf("FizzBuzz");
+else if (n % 3 == 0)
+printf("Fizz");
+else if (n % 5 == 0)
+printf("Buzz");
+else
+printf("%d", n);
+if (n != 100)
+printf(" ");
+}
+printf("\n");
+return 0;
 }
