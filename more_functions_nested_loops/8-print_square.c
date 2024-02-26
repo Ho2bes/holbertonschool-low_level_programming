178a2bf99497e92cafc4b73975b0d9d98e6f7edd
Ho2bes <8696@holbertonstudents.com>
2024-02-26 14:32:54 +0000

    8-print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -2,24 +2,24 @@
 
 /**
 *print_square - Compute the absolute value of an integer
-*@n : - variable
+*@size : - variable
 * Return: The absolute value
 */
 
 void print_square(int size)
 {
 
-	int a;
-	int b;
-	if (size > 0)
-	{
-		for (a = 0; a < size; a++)
-				{
-					for (b = 0; b < size; b++)
-					_putchar('#');
-					_putchar('\n');
-				}
-	}
-	else
-	_putchar('\n');
+int a;
+int b;
+if (size > 0)
+{
+for (a = 0; a < size; a++)
+{
+for (b = 0; b < size; b++)
+_putchar('#');
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
 }
