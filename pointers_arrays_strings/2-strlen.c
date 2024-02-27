59699094fc93c9a95dad8f441818f7525bc22ccc
Ho2bes <8696@holbertonstudents.com>
2024-02-27 11:50:25 +0000

    2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,14 @@
 
 /**
  *_strlen - returns the length of a string
- * @s: string
+ *@s: string
  *Return: returns lenght;
  */
 
 int _strlen(char *s)
 {
-	int count, inc;
-	inc = 0;
+int count, inc;
+inc = 0;
 	for (count = 0; s[count] != '\0'; count++)
 		inc++;
 	return (inc);
