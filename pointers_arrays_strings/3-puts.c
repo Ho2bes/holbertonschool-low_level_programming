67f16b3c7dfffddfcc364eb19e0ad6edf7090d2f
Ho2bes <8696@holbertonstudents.com>
2024-02-27 12:14:59 +0000

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- *_puts
- *@str : variable
+ *_puts-print a string followed by a new line
+ *@str : strings
+ *Return: void
  */
 
 void _puts(char *str)
 {
 
-	while (*str != '\0') {
-    _putchar(*str);
-    str++;
+while (*str != '\0')
+	{
+_putchar(*str);
+str++;
 }
-	_putchar('\n');
+_putchar('\n');
 }
