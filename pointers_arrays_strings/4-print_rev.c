2ccbddc71f6380a8a444a690cf36cdbcabee3ed5
Ho2bes <8696@holbertonstudents.com>
2024-02-27 13:20:06 +0000

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,10 +9,10 @@
 
 void print_rev(char *s)
 {
-	int i;
-	for (i = _strlen(s)-1; i >= '\0'; i--)
-		
-	{_putchar(s[i]);
-	}
-	_putchar('\n');
+int i;
+for (i = _strlen(s) - 1; i >= '\0'; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
 }
