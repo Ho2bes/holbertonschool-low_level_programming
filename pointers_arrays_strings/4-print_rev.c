d6b8697416f3169509321541f2e60f0d84e4808c
Ho2bes <8696@holbertonstudents.com>
2024-02-27 13:10:59 +0000

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,17 @@
 #include "main.h"
+#include "2-strlen.c"
 
 /**
  *print_rev a string followed by a new line
- *@str : strings
+ *@s : strings
  *Return: void
  */
 
-void _puts(char *str)
+void print_rev(char *s)
 {
 	int i;
-	for (i = _strlen(str) -1; i >= '\0'; i--)
-	{_putchar(str[i]);
+	for (i = _strlen(s) -1; i >= '\0'; i--)
+	{_putchar(s[i]);
 	}
 	_putchar('\n');
 }
