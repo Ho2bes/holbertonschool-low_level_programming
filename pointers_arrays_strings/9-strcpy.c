0c60a96e90b98ad2c07200c7bf91a29714650aeb
Ho2Bes <8696@holbertonstudents.com>
2024-02-28 15:04:01 +0000

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * _strcpy - copy string in string
+ *@dest: string copied
+ *@src: string to copy
+ *Return: string copy
+ */
+
+char *_strcpy(char *dest, char *src)
+{ int i;
+	for (i = o; i <= _strlen(src); i++)
+	{
+		dest[i] = src[i]
+			}
+	return (dest);
+			}
