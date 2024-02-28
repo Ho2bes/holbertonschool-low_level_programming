eca6ec845adbc8b0d1d2a8cc5e7940a4617cc998
Ho2Bes <8696@holbertonstudents.com>
2024-02-28 14:00:33 +0000

    8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_arrray - print array
+ *@n : is the number of elemet of the array
+ *@a : is the pointer
+ *Return: success
+ */
+
+void print_array(int *a, int n)
+
+{
+	int i;
+	for(i = 0; i < n; i++)
+	{
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
+	}
+	printf("\n");
+}
