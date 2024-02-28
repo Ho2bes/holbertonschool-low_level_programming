01f9d12d4f380792bdadf250245513df56478703
Ho2Bes <8696@holbertonstudents.com>
2024-02-28 11:41:09 +0000

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,14 +8,8 @@
 
 void puts2(char *str)
 {
-	int i = 0;
-while (*str != '\0')
-	{
-_putchar(*str);
-str++;
-}
-
-if (i != '\0')
-	_putchar(i + 2);
-_putchar('\n');
+	int i;
+	for (i = 0; i != '\=0'; i++)
+		if (i % 2 = 0)
+		_putchar(str[i]);
 }
