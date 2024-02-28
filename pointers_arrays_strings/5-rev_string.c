66dfab492681b3622f8e35308811a11df7bea6a0
Ho2Bes <8696@holbertonstudents.com>
2024-02-28 07:59:08 +0000

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ *rev_string - reverse a string
+ *
+ *Return: void
+ */
+
+void rev_string(char *s)
+{
+
+while (*s != '\0')
+	{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
+void rev_string(char *s)
+	{
+int i;
+for (i = _strlen(s) - 1; i >= '\0'; i--)
+{
+_putchar(s[i]);
+}
