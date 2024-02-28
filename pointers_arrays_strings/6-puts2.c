d6c7c1d6205de413a5a6d399ef0092c60f58eeca
Ho2Bes <8696@holbertonstudents.com>
2024-02-28 11:46:53 +0000

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -2,14 +2,14 @@
 
 /**
  *puts2 - prints every other character
- *
- *
+ * @i: variable
+ *Return: void 
  */
 
 void puts2(char *str)
 {
 	int i;
-	for (i = 0; i != '\=0'; i++)
-		if (i % 2 = 0)
+	for (i = 0; i != '\0'; i++)
+		if (i % 2 == 0)
 		_putchar(str[i]);
 }
