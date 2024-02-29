2a3f4678169dbde51a981bc7a0f7db6263cbaf0a
Ho2bes <8696@holbertonstudents.com>
2024-02-29 12:34:49 +0000

    begin of the projet 3

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -12,12 +12,12 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j;
 	char *temp;
-	for (i = _strlen(dest) ; i = '\0'; i++)
-	for (j = _strlen(src) ; j = '\0'; j++)
+	for (i = _strlen(dest); i = '\0'; i++)
+	for (j = _strlen(src); j = '\0'; j++)
 
 		{
 			*temp = (dest[i] - 1) + src[j];
 		}
 	
-	return (*temp);
+	return (char);
 }
