d03ab6d39f764cf948292d78d38dc0ab1a2876fb
Ho2bes <8696@holbertonstudents.com>
2024-02-29 14:02:38 +0000

    1-strncat.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ **_strncat - concatenates two strings
+ *@dest: first string
+ *@src: second string
+ *@n: variable of strncat
+ *Return: success
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0;
+int j = 0;
+while  (dest[i] != '\0')
+i++;
+while (src[j] != '\0' && j < n)
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+return (dest);
+}
