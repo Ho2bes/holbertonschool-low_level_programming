b4c18a4d53bc4d94f42197518ff0538507477963
Ho2bes <8696@holbertonstudents.com>
2024-03-01 12:41:14 +0000

    5-string_toupper.c

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -2,19 +2,17 @@
 
 /**
  **string_toupper - changes all lowercase
- *@i : variable
- *@c : variable
  *@s : pointer
  *Return: string in lowercase
  */
 
 char *string_toupper(char *s)
 {
-  int i;
-  for (i = 0; s[i] != '\0'; i++)
-  {
-    if (s[i] >= 'a' && s[i] <= 'z')
-	    s[i] = s[i] -32;
-  }
-  return (s);
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] >= 'a' && s[i] <= 'z')
+s[i] = s[i] - 32;
+}
+return (s);
 }
