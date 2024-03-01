8e0dff913e967cc5b66f3edc64ca2bce0cba723f
Ho2bes <8696@holbertonstudents.com>
2024-03-01 11:10:10 +0000

    4-rev_array.c

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -8,14 +8,13 @@
 
 void reverse_array(int *a, int n)
 {
-	int i;
-	char temp;
+	int i, tmp;
 
 	for (i = 0; i < n; i++)
 	{
-		temp = a[i];
-		a[i] = a[n -1];
-		a[n -1] = temp;
+		tmp = a[i];
+		a[i] = a[n - 1];
+		a[n - 1] = tmp;
 		n--;
 	}
 }
