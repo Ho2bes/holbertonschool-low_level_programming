00706b4884e5394b7c394731ece25b47f4e3d780
Ho2bes <8696@holbertonstudents.com>
2024-03-04 10:09:40 +0000

    0-memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ **_memset - fills memory with a constant byte
+ *@s: pointer
+ *@b: constant byte
+ *@n: number of characters
+ *Return: Success
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned i;
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
+}
