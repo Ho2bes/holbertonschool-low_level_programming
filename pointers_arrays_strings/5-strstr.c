585115d7488a66b317c1088f4bba3afa2e1309a9
Ho2bes <8696@holbertonstudents.com>
2024-03-06 08:26:26 +0000

    5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -10,15 +10,15 @@ char *_strstr(char *haystack, char *needle)
 {
 {
 int i, j;
-for (i = 0; *haystack != '\0'; i++)
+for (i = 0; haystack[i] != '\0'; i++)
 {
 for (j = 0; needle[j] != '\0'; j++)
 {
-if (*haystack == *needle)
-return (haystack);
+if (haystack[i + j] == needle [j])
+break;
 }
-haystack++;
+if (!needle[j])
+return (&haystack[i]);
 }
 return ('\0');
-}
 }
