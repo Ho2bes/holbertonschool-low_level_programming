4b4d44be8daa83a6f21247853de09016d6a67f47
Ho2bes <8696@holbertonstudents.com>
2024-03-06 11:45:49 +0000

    begin of 3

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main - name
+*@argc: size of array
+*@argv: array
+*Return: Success
+*/
+
+int main (int argc, char argv[])
+{
+if (argc > 0)
+printf("%s\n", *argv);
+return (0);
+}
