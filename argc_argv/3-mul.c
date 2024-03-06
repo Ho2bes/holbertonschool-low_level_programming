d9cdf0d36ee078da75cff94f4c91911b5ff051c3
Ho2bes <8696@holbertonstudents.com>
2024-03-06 13:10:54 +0000

    3-mul.c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main - name
+*@argc: size of array
+*@argv: array
+*Return: Success
+*/
+
+int main(int argc, char *argv[])
+{
+if (argc >= 3)
+{
+int i = atoi(argv[1]);
+int j = atoi(argv[2]);
+int resultat = i * j;
+printf("%d\n", resultat);
+}
+return (0);
+}
