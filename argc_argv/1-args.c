a4bf4b70d4de365059e697bd6632f13444fb1006
Ho2bes <8696@holbertonstudents.com>
2024-03-06 12:07:28 +0000

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stlib.h>
+
+/**
+*main - name
+*@argc: number of argument
+*@argv: array
+*Return: Success
+*/
+
+int main(int argc, char **argv)
+{
+printf("%d\n", argc);
+return (0);
+}
