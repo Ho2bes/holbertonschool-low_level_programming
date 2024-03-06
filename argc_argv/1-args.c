b5d4e4bb94daea2761356cda71b7e77450416ea1
Ho2bes <8696@holbertonstudents.com>
2024-03-06 12:38:42 +0000

    1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -3,15 +3,15 @@
 #include <stdlib.h>
 
 /**
-*main - name
-*@argc: number of argument
-*@argv: array
-*Return: Success
+* main - check .
+* @argc: The size of the argv array.
+* @argv: An array
+* Return: Always argc.
 */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 if (*argv[0] > 0)
-printf("%i\n", argc -1);
+printf("%i\n", argc - 1);
 return (0);
 }
