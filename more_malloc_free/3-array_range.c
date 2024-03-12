cc2a95d0bc23b03ee1a51dc1d868a1d8c113aa9a
Ho2bes <8696@holbertonstudents.com>
2024-03-12 13:39:29 +0000

    3-array_range.c

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ **array_range - array
+ *@min: variable min
+ *@max: variable max
+ *Return: Succcess
+ */
+
+int *array_range(int min, int max)
+{
+int i, j = 0;
+int *array;
+if (min > max)
+return (NULL);
+array = malloc(max - min + 1);
+if (array == NULL)
+return (NULL);
+for (i = min; i <= max; i++, j++)
+array[j] = i;
+return (array);
+}
