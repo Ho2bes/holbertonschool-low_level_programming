ba9029b35ea417e3de90eda8f97bae69089a84bd
Ho2bes <8696@holbertonstudents.com>
2024-03-13 10:23:09 +0000

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *struct dog - structure
+ *@name: name
+ *@age: age
+ *@owner : owner
+ */
+
+typedef struct dog
+{
+	signed char name[100];
+	int age;
+	signed char owner[100]
+} my_dog;
