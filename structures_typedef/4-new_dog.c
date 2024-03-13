8d316425732a0a26392deab6eb3417e75a8b2297
Ho2bes <8696@holbertonstudents.com>
2024-03-13 14:17:54 +0000

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,13 +10,11 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-struct dog *d;
-d = malloc(sizeof(struct dog));
+struct dog *d  = malloc(sizeof(struct dog));
 if (d == NULL)
 return (NULL);
 d->name = name;
 d->age = age;
 d->owner = owner;
 return (d);
-free(d);
 }
