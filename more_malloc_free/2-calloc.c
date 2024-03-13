719e7e9612691cf2ded98d4d695f7c19b20b0f66
Ho2bes <8696@holbertonstudents.com>
2024-03-13 08:39:00 +0000

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -2,24 +2,23 @@
 #include <stdlib.h>
 
 /**
- **_calloc - calloc
- *@nmemb: array
- *@size: variable
- *Return: Success
- */
+*_calloc - calloc
+*@nmemb: array
+*@size: variable
+*Return: Success
+*/
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-unsigned int i;
-char *array;
-if (size == 0 || nmemb == 0)
-return (NULL);
-array = malloc(size * nmemb);
-if (array == NULL)
-return (NULL);
-i = 0;
-for (i = 0; i < nmemb; i++)
-*(array + (i * size)) = 0;
-return (array);
-free(array);
+	unsigned int i;
+	char *array;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	array = malloc(nmemb * size);
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		*(array + (i * size)) = 0;
+	return (array);
 }
