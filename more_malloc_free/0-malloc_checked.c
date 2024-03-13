efc477ef32aee4cb5fb9984ed57ced802660bfe5
Ho2bes <8696@holbertonstudents.com>
2024-03-13 08:26:52 +0000

    0-malloc_checked.c

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 
 /**
-**malloc_checked - checked
+*malloc_checked - checked
 *@b: variable
 *Return: Success
 */
 
 void *malloc_checked(unsigned int b)
 {
-void *p = malloc(b);
+void *p
+p = malloc(b);
 if (p == NULL)
 exit(98);
 return(p);
