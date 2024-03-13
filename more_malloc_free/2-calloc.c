d16519531e1d999d4c45af82cc3e4ef1a4321449
Ho2bes <8696@holbertonstudents.com>
2024-03-13 08:43:12 +0000

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -9,20 +9,20 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ptr;
-	unsigned int count;
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
-	{
-		return (NULL);
-	}
-	count = 0;
-	while (count < nmemb * size)
-	{
-		ptr[count] = 0;
-		count++;
-	}
-	return (ptr);
+char *ptr;
+unsigned int count;
+if (nmemb == 0 || size == 0)
+return (NULL);
+ptr = malloc(nmemb * size);
+if (ptr == NULL)
+{
+return (NULL);
+}
+count = 0;
+while (count < nmemb * size)
+{
+ptr[count] = 0;
+count++;
+}
+return (ptr);
 }
