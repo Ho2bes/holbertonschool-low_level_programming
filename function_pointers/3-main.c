4193bae5285433f67c61d5d73f96f6d2ae2e0722
Ho2bes <8696@holbertonstudents.com>
2024-03-19 07:24:55 +0000

    task 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -9,29 +9,29 @@
  * Return: Always 0.
  */
 int main (int argc, char * argv[]);
-
+{
 int num1, num2;
 char *op;
-
+int result;
 if (argc != 4)
 {
 printf("Error\n");
 exit (98);
 }
-
 num1 = atoi(argv[1]);
 * op = argv[2];
 num2 = atoi(argv[3]);
-
 if (argv[2] == NULL)
 {
 printf("Error\n");
 exit (99);
 }
-
-if (argv[2] == '/' || argv[2] == '%' && argv[3] == 0)
+if ((argv[2] == '/' || argv[2] == '%') && argv[3] == 0)
 {
 printf("Error\n");
-exit (10)
+exit (100)
 }
-printf("%d\n", 
+result = get_op_func(atoi(argv[1]), atoi(argv[3]));
+printf("%d\n", result);
+return (0);
+}
