4246ec884ce26d7cad4aa2fcd708149468dade66
Ho2bes <8696@holbertonstudents.com>
2024-03-19 09:47:16 +0000

    task 3

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
@@ -9,5 +8,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+if (name == NULL || f == NULL)
+return;
 f(name);
 }
