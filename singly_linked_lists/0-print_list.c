457f41e62812b656fa2361dba96a029a490cb1cb
Ho2bes <8696@holbertonstudents.com>
2024-04-01 12:25:30 +0000

    modification of 0-print_list.c with good code

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,1 +1,24 @@
 #include "lists.h"
+
+/**
+ * print_list -  prints all the elements of a list_t list.
+ * @h: pointer to list.
+ * Return: number of nodes.
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int count = 0;
+	const list_t *tmp_node = h;
+
+	while (tmp_node)
+	{
+		if (tmp_node->str)
+			printf("[%u] %s\n", tmp_node->len, tmp_node->str);
+		else
+			printf("[0] (nil)\n");
+		tmp_node = tmp_node->next;
+		count++;
+	}
+	return (count);
+}
