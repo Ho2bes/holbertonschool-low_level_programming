86fbe52eb4762a8829e3e90de50f3daea60f9c37
Ho2bes <8696@holbertonstudents.com>
2024-04-04 07:55:46 +0000

    create 3-set_bit.c

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * set_bit -  set value o  bit to 1 in index.
+ * @n: decimal number
+ * @index: bit to be return
+ * Return: decimal selected
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = 1 << index;
+
+	if (index <= (sizeof(n) * 8) - 1)
+	{
+		*n |= mask;
+		return (1);
+	}
+	return (-1);
+}
