09e01b57c3eafaf2e2b1436c2c19438ba0a85759
Ho2bes <8696@holbertonstudents.com>
2024-04-05 10:43:19 +0000

    create 2-append_text_to_file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -11,18 +11,22 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd;
 	size_t length = 0;
 
-	if (!filename || !text_content)
+	if (!filename)
 	{
 		return (-1);
 	}
 	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
+	if (text_content == NULL)
 	{
-		return (-1);
+		text_content = "";
 	}
 	while (text_content[length] != '\0')
 	{
 		length++;
+	}
+	if (fd == -1)
+	{
+		return (-1);
 	}
 	if (write(fd, text_content, length) == -1)
 	{
